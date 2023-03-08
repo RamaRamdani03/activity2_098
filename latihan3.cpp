aaac1445b11b041c7bb28d646fe094970def9b39
RamaRamdani03 <ramaramdani449@gamil.com>
2023-03-08 12:16:31 +0700

    first commit

---
+++ latihan3.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+double P, L;
+
+void input(){
+    cout << "Masukan Panjangnya = ";
+    cin >> P;
+    cout << "Masukan Lebarnya = ";
+    cin >> L;
+}
+
+double proses(){
+    return P*L;
+}
+
+double proses2(double a, double b){
+    return a*b;
+}
+
+void ouput(){
+    cout << "Luasnya = " << proses();
+    cout << "\nLuasnya ke 2 = " << proses2(P,L);
+}
+
+
+int main(){
+    input();
+    ouput();
+}
